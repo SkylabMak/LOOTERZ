513019d7779a58d834e6db27e1615260b64a8a3a
praisan <skylab135@gmail.com>
2024-06-27 09:07:39 +0700

    config typings

---
+++ src/app/started/testData.ts
@@ -0,0 +1,10 @@
+import { ShowRoom } from "@/typings"
+export const testData: ShowRoom = {
+    roomName: "",
+    roomID: "",
+    NumberPlayers: 9,
+    currentPlayes:8,
+    time:8,
+    privateStatus:false,
+}
+
