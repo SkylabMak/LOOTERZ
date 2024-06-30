630fc6bbc87ce7e970efd408feeb450a89af3a93
mickie2k <63552271+mickie2k@users.noreply.github.com>
2024-06-30 17:23:53 +0700

    Change nunito sans to nunito

--- src/utilites/font.tsx
+++ src/utilites/font.tsx
@@ -1,5 +1,6 @@
-import { Nunito_Sans, Paytone_One } from "next/font/google";
-export const nunito = Nunito_Sans({
+import { Nunito, Paytone_One } from "next/font/google";
+export const nunito = Nunito({
+	weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
 	subsets: ["latin"],
 	display: "swap",
 });
