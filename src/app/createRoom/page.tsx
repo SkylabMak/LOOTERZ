fda3db2e1ce885cbebe50fa029bc7f4d6080a23f
Garaiiiii <toriyakikuributa@gmail.com>
2024-07-21 22:23:10 +0700

    createRoom page with button that can increase and decrease input

--- src/app/createRoom/page.tsx
+++ src/app/createRoom/page.tsx
@@ -2,9 +2,10 @@
 
 import ImageCard from "../../image/looterzCard.png";
 import TextFill from "@/component/textFill";
+import NumberFill from "@/component/fillNumber"
 export default function createRoom() {
     return (
-        <div className="bg-gradient-to-b from-[#1B131D] to-[#120D17]">
+        <div className="bg-gradient-to-b from-[#1B131D] to-[#120D17] min-h-screen">
             <div className=" flex justify-between pr-20">
                 <button className="bg-black text-left text-lg mt-6 ml-32 p-5 rounded-tl-lg hover:bg-white hover:text-black">
                     Create a Room
@@ -13,18 +14,22 @@ export default function createRoom() {
                     username
                 </button>
             </div>
-            <div className="bg-gradient-to-b from-[#1B131D] to-[#120D17] grid grid-cols-2 px-32">
-                <div className="bg-black border border-red-50 rounded-t-lg">
-                    <p className="text-2xl">Setting</p>
-                    <div className="">
-                        <div className="mt-12 space-y-8 flex flex-col items-center bg-gradient-to-b from-[#1B131D] to-[#120D17] px-4">
+            <div className="bg-gradient-to-b from-[#1B131D] to-[#120D17] grid grid-cols-2 px-32 ">
+                <div className="bg-[#18181B] rounded-lg">
+                    <div className="bg-black h-[90px] pl-[64px] flex items-center rounded-t-lg">
+                        {/* <p className="text-2xl px-4 h-4">Setting</p> */}
+                        <span className ="text-2xl">Settings</span>
+                    </div>
                     
-                            <TextFill name={"Room name"} value={"text"} />
-                            <TextFill name={"Players"} value = {"number"}/>
-                            <TextFill name={"Time/Turns"} value = {"number"}/>
-                            <TextFill name={"Password"} value = {"text"}/>
+                    <div className="">
+                        <div className="mt-4 space-y-8 flex flex-col items-center px-4">
+                    
+                            <TextFill name={"Room name"}/>
+                            <NumberFill name={"Players"} Step={"1"} Min={"2"} Max={"6"}/>
+                            <NumberFill name={"Time/Turns"} Step={"10"} Min={"30"} Max={"120"}/>
+                            <TextFill name={"Password"}/>
                         </div>
-                        <div className="py-4 flex justify-end mr-10 space-x-5 bg-gradient-to-b from-[#1B131D] to-[#120D17] w-full pr-4">
+                        <div className="py-4 flex justify-end mr-10 space-x-5 w-full pr-4">
                             <button className="bg-black text-gray rounded-3xl border border-gray-500 px-5 py-2">Cancel</button>
                             <button className="bg-Accent1 text-gray rounded-3xl border border-gray-500 px-5 py-2">Create a Room</button>
                         </div>
--- src/component/textFill.tsx
+++ src/component/textFill.tsx
@@ -2,17 +2,21 @@ import * as React from 'react';
 
 interface ITextFillProps {
     name: string;
-    value: string;
 }
 
 const TextFill: React.FunctionComponent<ITextFillProps> = (props) => {
-    const { name, value } = props;
+    const { name} = props;
+    // const isNumber: boolean = value === 'number';
+
     return (
+
         <div className=" w-full">
             <p>{name}</p>
-            <input style={{background : "#B8B8B8",padding : "20px"}} className="rounded-lg text-white w-full p-[100px]" type={value} placeholder="Placeholder" />
+            <input style={{ background: "#B8B8B8", padding: "10px" }} className="rounded-lg text-black w-full p-[100px]" type="text" placeholder="Placeholder" /> {/* cant use (bg, p, h, w) */}
         </div>
     );
 };
 
 export default TextFill;
+
+// disabled={isNumber?true:false} min={isNumber?2:''} max={isNumber?6:1}
